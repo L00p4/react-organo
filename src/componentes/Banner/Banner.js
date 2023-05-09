ccc4877773b6e1ec78bab5e564b67bb79a3f8c65
Guilherme <guilherme.marucchi@gmail.com>
2023-05-08 21:14:33 -0300

    Finaliza primeira parte

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX -> "parece HTML, mas não é (eita!)"
+    return (
+        <>
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página do Organo."></img>
+        </header>
+        <h1>Teste</h1>
+        </>
+    )
+}
